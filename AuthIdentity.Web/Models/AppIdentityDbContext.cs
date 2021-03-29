308a1bf0ce3c6abdb6730393bedd5f827b05ad2c
Asus <Asus@DESKTOP-7QSBH7L>
2021-03-29 20:55:35 +0300

    Add project files.

---
+++ AuthIdentity.Web/Models/AppIdentityDbContext.cs
@@ -0,0 +1,20 @@
+﻿using Microsoft.AspNetCore.Identity;
+using Microsoft.AspNetCore.Identity.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace AuthIdentity.Web.Models
+{
+    public class AppIdentityDbContext : IdentityDbContext<AppUser>
+    {
+        public AppIdentityDbContext(DbContextOptions<AppIdentityDbContext> opt) : base(opt)
+        {
+
+        }
+
+
+    }
+}
