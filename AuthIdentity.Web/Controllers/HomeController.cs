d0c0155669bfab11649afb191cfe4f99a23c3554
Asus <Asus@DESKTOP-7QSBH7L>
2021-04-01 20:54:54 +0300

    Kullanıcı üye olma ve client side validation

--- AuthIdentity.Web/Controllers/HomeController.cs
+++ AuthIdentity.Web/Controllers/HomeController.cs
@@ -1,4 +1,7 @@
-﻿using Microsoft.AspNetCore.Mvc;
+﻿using AuthIdentity.Web.Models;
+using AuthIdentity.Web.ViewModels;
+using Microsoft.AspNetCore.Identity;
+using Microsoft.AspNetCore.Mvc;
 using System;
 using System.Collections.Generic;
 using System.Linq;
@@ -8,9 +11,63 @@ namespace AuthIdentity.Web.Controllers
 {
     public class HomeController : Controller
     {
+        public UserManager<AppUser> _userManager;
+
+        public HomeController(UserManager<AppUser> userManager)
+        {
+            _userManager = userManager;
+        }
+
         public IActionResult Index()
         {
             return View();
         }
+
+        public IActionResult Login()
+        {
+            return View();
+        }
+
+        [HttpPost]
+        public IActionResult Login(UserViewModel userViewModel)
+        {
+            return View();
+        }
+
+        public IActionResult SignUp()
+        {
+            return View();
+        }
+
+        [HttpPost]
+        public async Task<IActionResult> SignUp(UserViewModel userViewModel)
+        {
+            if (ModelState.IsValid)
+            {
+                AppUser user = new AppUser();
+                user.UserName = userViewModel.UserName;
+                user.Email = userViewModel.Email;
+                user.PhoneNumber = userViewModel.PhoneNumber;
+
+                IdentityResult result = await _userManager.CreateAsync(user,userViewModel.Password);
+
+                if (result.Succeeded)
+                {
+                    return RedirectToAction("Login");
+                }
+                else
+                {
+                    foreach (IdentityError err in result.Errors)
+                    {
+                        ModelState.AddModelError("",err.Description);
+                    }
+                }
+
+
+            }
+
+            return View(userViewModel);
+        }
+
     }
 }
---
+++ AuthIdentity.Web/ViewModels/UserViewModel.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace AuthIdentity.Web.ViewModels
+{
+    public class UserViewModel
+    {
+        [Required(ErrorMessage ="Kullanıcı ismi gereklidir.")]
+        [Display(Name ="Kullanıcı Adı")]
+        public string UserName { get; set; }
+
+        [Display(Name ="Tel No")]
+        public string PhoneNumber { get; set; }
+
+        [Required(ErrorMessage = "Email adresi gereklidir.")]
+        [Display(Name = "Email")]
+        [EmailAddress(ErrorMessage ="Geçersiz mail adresi")]
+        public string Email { get; set; }
+
+        [Required(ErrorMessage = "Parola gereklidir.")]
+        [Display(Name = "Parola")]
+        [DataType(DataType.Password)]
+        public string Password { get; set; }
+    }
+}
