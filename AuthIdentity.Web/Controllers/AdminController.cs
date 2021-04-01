d186c8541d150b9e2ec81427a8c03a3a994f82bf
Asus <Asus@DESKTOP-7QSBH7L>
2021-04-01 16:19:33 +0300

    AdminController oluşturuldu. AdminLayout,Index,ViewImport eklendi

---
+++ AuthIdentity.Web/Controllers/AdminController.cs
@@ -0,0 +1,25 @@
+﻿using AuthIdentity.Web.Models;
+using Microsoft.AspNetCore.Identity;
+using Microsoft.AspNetCore.Mvc;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace AuthIdentity.Web.Controllers
+{
+    public class AdminController : Controller
+    {
+        private UserManager<AppUser> _userManager;
+
+        public AdminController(UserManager<AppUser> userManager)
+        {
+            _userManager = userManager;
+        }
+
+        public IActionResult Index()
+        {
+            return View(_userManager.Users.ToList());
+        }
+    }
+}
